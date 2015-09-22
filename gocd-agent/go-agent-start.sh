a51e438a34ac924473c62c7371074a51f368c0b9
Attila Maczak <attila@maczak.hu>
2015-09-22 12:02:43 -0400

    ansible 2.0 support + built in ssh key support

--- gocd-agent/go-agent-start.sh
+++ gocd-agent/go-agent-start.sh
@@ -12,4 +12,12 @@ echo "agent.auto.register.key=$AGENT_KEY" >/var/lib/go-agent/config/autoregister
 if [ -n "$AGENT_RESOURCES" ]; then echo "agent.auto.register.resources=$AGENT_RESOURCES" >>/var/lib/go-agent/config/autoregister.properties; fi
 if [ -n "$AGENT_ENVIRONMENTS" ]; then echo "agent.auto.register.environments=$AGENT_ENVIRONMENTS" >>/var/lib/go-agent/config/autoregister.properties; fi
 
+if [ -d /tmp/ssh ]
+then
+  echo -e "Copying SSH folder to /var/go"
+  cp -r /tmp/ssh /var/go/.ssh
+  chown -R go:go /var/go/.ssh
+  chmod 400 /var/go/.ssh/id*
+fi
+
 exec /usr/share/go-agent/agent.sh
